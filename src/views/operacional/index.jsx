3d9db9ff0a1156e436282d8482d990db95d631e3
waysterMelo <waystermelo@gmail.com>
2025-03-20 16:09:37 -0300

    20-03-2025 tela ajustada

--- src/views/operacional/index.jsx
+++ src/views/operacional/index.jsx
@@ -45,8 +45,8 @@ const ControleOperacional = () => {
     return (
         <Box pt={{ base: "90px", md: "50px", xl: "5%" }} ml={{ base: "2%" }}>
             <Grid
-                gridTemplateColumns={'repeat(1, 1fr)'}
-                gap={{ base: "20px", xl: "20px" }}
+                emplateColumns={{ base: "1fr", sm: "repeat(2, 1fr)", md: "repeat(3, 1fr)", lg: "repeat(4, 1fr)" }}
+                gap={4}
                 display={{ base: "block", xl: "grid" }}>
                 <Banner url_voltar={'/admin/home'} texto_primario={'CONTROLE OPERACIONAL'}
                     texto_secundario={'Abaixo confira as informações inseridas'}
@@ -54,7 +54,7 @@ const ControleOperacional = () => {
                     primeiro_botao={'CARGAS | PRESSÃO | TEMPERATURA | SONDA'}
                     url={'cargas-pressao-temperatura-sonda'}
                     primeiro_botao_texto_secundario={'RITMO, MÉDIAS DA UMIDADE E DENSIDADE, MÉDIA HORA'}
-                    
+
                     segundo_botao={'CONTROLE CARVÃO'} segundo_botao_texto_secundario={'PESO CALCULADO | PESO ENFORNADO | MÉDIA | CONSUMO'} url_segundo_botao={'controle-carvao'}
 
                         terceiro_botao={'RESERVAS'}
@@ -64,137 +64,153 @@ const ControleOperacional = () => {
             </Grid>
 
             <form onSubmit={handleSubmit}>
-                <SimpleGrid columns={{ base: 1, md: 1, xl: 1 }} spacing={3} mt={4}>
-                    <Stack direction={['column', 'row']}>
-                        <Box height="auto" bg="white" p="4" boxShadow="xs" rounded="md" flex="0.5">
+                <SimpleGrid columns={{ base: 1, sm: 2,  md: 4, xl: 1 }}  spacing={{ base: 3, md: 4 }} width={'100%'}>
+                    <Stack direction={['column', 'rw']}>
+                        <Box height="auto" bg="white" p="4" boxShadow="xs" rounded="md" width={{ base: "100%", sm: "100%", md: "100%", lg: "100%" }} >
                             <Text className="p-3 text-bg-dark text-center">Operacional</Text>
                             <Flex gap={3} flexWrap="nowrap" p={4}>
                                 <FormControl id="a" className="form-control">
                                     <FormLabel className="fw-bold text-center">A</FormLabel>
-                                    <Input type="text" bgColor="yellow.100" className={'text-center'} name={'a'} value={formData.a} onChange={handleChange}/>
+                                    <Input type="text" bgColor="yellow.300" className={'text-center'} name={'a'} value={formData.a} onChange={handleChange}/>
                                 </FormControl>
                                 <FormControl id="gaiola" className="form-control">
                                     <FormLabel className="fw-bold text-center">Gaiola</FormLabel>
-                                    <Input type="text" bgColor="yellow.100" className={'text-center'} name={'gaiola'} value={formData.gaiola} onChange={handleChange}/>
+                                    <Input type="text" bgColor="yellow.300" className={'text-center'} name={'gaiola'} value={formData.gaiola} onChange={handleChange}/>
                                 </FormControl>
                                 <FormControl id="cargaSeca" className="form-control">
                                     <FormLabel className="fw-bold">Carga/Seca</FormLabel>
-                                    <Input type="text" bgColor="yellow.100" className={'text-center'} name={'cargaSeca'} value={formData.cargaSeca} onChange={handleChange}/>
+                                    <Input type="text" bgColor="yellow.300" className={'text-center'} name={'cargaSeca'} value={formData.cargaSeca} onChange={handleChange}/>
                                 </FormControl>
 
                                 <FormControl id="carga-hora" className="form-control">
                                     <FormLabel className="fw-bold">Carga/Hora</FormLabel>
-                                    <Input type="text" bgColor="yellow.100" className={'text-center'} name={'cargaHora'} value={formData.cargaHora} onChange={handleChange}/>
+                                    <Input type="text" bgColor="yellow.300" className={'text-center'} name={'cargaHora'} value={formData.cargaHora} onChange={handleChange}/>
                                 </FormControl>
 
                                 <FormControl id="vazao" className="form-control">
                                     <FormLabel className="fw-bold text-center">Vazão</FormLabel>
-                                    <Input type="text" bgColor="yellow.100" name={'vazao'} value={formData.vazao} onChange={handleChange}/>
+                                    <Input type="text" bgColor="yellow.300" name={'vazao'} value={formData.vazao} onChange={handleChange}/>
                                 </FormControl>
                             </Flex>
                         </Box>
-                        <Box height="auto" bg="white" p={4} boxShadow="xs" rounded="md" flex="0.18">
-                            <Text className="p-3 text-bg-dark text-center">Pressão</Text>
-                            <HStack className="p-3">
-                                <FormControl className="form-control">
-                                    <FormLabel className={'fw-bold'}>Coroa</FormLabel>
-                                    <InputMask
-                                        mask={'9.99'}
-                                        value={formData.pressaoCoroa}
-                                        onChange={handleChange}
-                                        placeholder={'0.00'}
-                                        name={'pressaoCoroa'}
-                                    >
-                                        {(inputProps) => <Input {...inputProps} type={'text'} bgColor={'yellow.100'} />}
-                                    </InputMask>
-                                </FormControl>
-                                <FormControl className="form-control">
-                                    <FormLabel className={'fw-bold'}>Topo</FormLabel>
-                                    <Input bgColor={'yellow.100'} name={'pressaoTopo'} value={formData.pressaoTopo} onChange={handleChange}/>
-                                </FormControl>
-                            </HStack>
-                        </Box>
-                        <Box height="auto" p={4} bg="white" boxShadow="xs" rounded="md" flex="0.18">
-                            <Text className="p-3 text-bg-dark text-center">Temperatura</Text>
-                            <HStack className="p-3">
-                                <FormControl className="form-control">
-                                    <FormLabel className={'fw-bold'}>Coroa</FormLabel>
-                                    <Input bgColor={'yellow.100'} name={'temperaturaCoroa'} value={formData.temperaturaCoroa} onChange={handleChange}/>
-                                </FormControl>
-                                <FormControl className="form-control">
-                                    <FormLabel className={'fw-bold'}>Topo</FormLabel>
-                                    <Input bgColor={'yellow.100'} name={'temperaturaTopo'} value={formData.temperaturaTopo} onChange={handleChange}/>
-                                </FormControl>
-                            </HStack>
-                        </Box>
-                        <Box height="auto" p={4} bg="white" boxShadow="xs" rounded="md" flex="0.12">
-                            <Text className="p-3 text-bg-dark text-center fw-bold">Sonda</Text>
-                            <HStack className="p-3">
-                                <FormControl className="form-control">
-                                    <FormLabel className={'fw-bold'}>Digite</FormLabel>
-                                    <Input type="text" bgColor="yellow.100" name={'sonda'} value={formData.sonda} onChange={handleChange}/>
-                                </FormControl>
-                            </HStack>
-                        </Box>
-                    </Stack>
+                        <Flex display="flex" gap={4} width="100%" flexWrap="wrap" justifyContent="space-between">
+                            {/* Pressão */}
+                            <Box
+                                height="auto"
+                                bg="white"
+                                p={4}
+                                boxShadow="xs"
+                                rounded="md"
+                                flex="1"
+                                minWidth="300px"
+                            >
+                                <Text className="p-3 text-bg-dark text-center">Pressão</Text>
+                                <HStack className="p-3">
+                                    <FormControl className="form-control">
+                                        <FormLabel className={'fw-bold'}>Coroa</FormLabel>
+                                        <InputMask
+                                            mask={'9.99'}
+                                            value={formData.pressaoCoroa}
+                                            onChange={handleChange}
+                                            placeholder={'0.00'}
+                                            name={'pressaoCoroa'}
+                                        >
+                                            {(inputProps) => <Input {...inputProps} type={'text'} bgColor={'yellow.300'} />}
+                                        </InputMask>
+                                    </FormControl>
+                                    <FormControl className="form-control">
+                                        <FormLabel className={'fw-bold'}>Topo</FormLabel>
+                                        <Input bgColor={'yellow.300'} name={'pressaoTopo'} value={formData.pressaoTopo} onChange={handleChange}/>
+                                    </FormControl>
+                                </HStack>
+                            </Box>
 
+                            {/* Temperatura */}
+                            <Box
+                                height="auto"
+                                p={4}
+                                bg="white"
+                                boxShadow="xs"
+                                rounded="md"
+                                flex="1"
+                                minWidth="300px"
+                            >
+                                <Text className="p-3 text-bg-dark text-center">Temperatura</Text>
+                                <HStack className="p-3">
+                                    <FormControl className="form-control">
+                                        <FormLabel className={'fw-bold'}>Coroa</FormLabel>
+                                        <Input bgColor={'yellow.300'} name={'temperaturaCoroa'} value={formData.temperaturaCoroa} onChange={handleChange}/>
+                                    </FormControl>
+                                    <FormControl className="form-control">
+                                        <FormLabel className={'fw-bold'}>Topo</FormLabel>
+                                        <Input bgColor={'yellow.300'} name={'temperaturaTopo'} value={formData.temperaturaTopo} onChange={handleChange}/>
+                                    </FormControl>
+                                </HStack>
+                            </Box>
+                            <Box height="auto" p={4} bg="white" boxShadow="xs" rounded="md" width={{base: "100%", sm: "50%", md: "50%", lg: "20%"}}>
+                                <Text className="p-3 text-bg-dark text-center fw-bold">Sonda</Text>
+                                <HStack className="p-3">
+                                    <FormControl className="form-control">
+                                        <FormLabel className={'fw-bold'}>Digite</FormLabel>
+                                        <Input type="text" bgColor="yellow.300" name={'sonda'} value={formData.sonda} onChange={handleChange}/>
+                                    </FormControl>
+                                </HStack>
+                            </Box>
+                        </Flex>
+                    </Stack>
+                </SimpleGrid>
+
+                <SimpleGrid columns={{ base: 1, sm: 2,  md: 4, xl: 1 }} py={3} spacing={{ base: 3, md: 4 }} width={'100%'}>
                     <Stack direction={['column', 'row']}>
-                        <Box height="auto" bg="white" p="4" boxShadow="xs" rounded="md" flex="0.1">
+                        <Box height="auto" bg="white" p="4" boxShadow="xs" rounded="md" >
                             <Text className="p-3 text-bg-dark text-center">Densidade</Text>
-                            <Flex gap={3} flexWrap="nowrap" p={4}>
+                            <Flex gap={3} flexWrap="wrap" p={4}>
                                 <FormControl id="densidadeKg" className="form-control">
                                     <FormLabel className="fw-bold">KG/M³</FormLabel>
-                                    <Input type="text" bgColor="yellow.100" name={'densidadeKg'} value={formData.densidadeKg} onChange={handleChange}/>
+                                    <Input type="text" bgColor="yellow.300" name={'densidadeKg'} value={formData.densidadeKg} onChange={handleChange}/>
                                 </FormControl>
                             </Flex>
                         </Box>
-                        <Box height="auto" bg="white" p="4" boxShadow="xs" rounded="md" flex="0.1">
+                        <Box height="auto" bg="white" p="4" boxShadow="xs" rounded="md" >
                             <Text className="p-3 text-bg-dark text-center">Umidade</Text>
                             <Flex gap={3} flexWrap="nowrap" p={4}>
                                 <FormControl id="umidade" className="form-control">
                                     <FormLabel className="fw-bold text-center">%</FormLabel>
-                                    <Input className={'text-center'} type={'number'} bgColor="yellow.100" name={'umidade'} value={formData.umidade} onChange={handleChange}/>
+                                    <Input className={'text-center'} type={'number'} bgColor="yellow.300" name={'umidade'} value={formData.umidade} onChange={handleChange}/>
                                 </FormControl>
                             </Flex>
                         </Box>
-                        <Box height="auto" bg="white" p="4" boxShadow="xs" rounded="md" flex="0.1">
+                        <Box height="auto" bg="white" p="4" boxShadow="xs" rounded="md" >
                             <Text className="p-3 text-bg-dark text-center">Gusa/C</Text>
                             <Flex gap={3} flexWrap="nowrap" p={4}>
                                 <FormControl id="gusaKg" className="form-control">
                                     <FormLabel className="fw-bold text-center">Kg</FormLabel>
-                                    <Input type="text" bgColor="yellow.100" name={'gusaKg'} value={formData.gusaKg} onChange={handleChange}/>
+                                    <Input type="text" bgColor="yellow.300" name={'gusaKg'} value={formData.gusaKg} onChange={handleChange}/>
                                 </FormControl>
                             </Flex>
                         </Box>
-                        <Box height="auto" bg="white" p="4" boxShadow="xs" rounded="md" flex="0.2">
+                        <Box height="auto" bg="white" p="4" boxShadow="xs" rounded="md">
                             <Text className="p-3 text-bg-dark text-center">Peso Carvão</Text>
                             <Flex gap={3} flexWrap="nowrap" p={4}>
-
-
-
                                 <FormControl id="gusaKg" className="form-control">
-                                    <FormLabel className="fw-bold text-center">Acumulado Kilos</FormLabel>
-                                    <Input className={'text-center'} type="text" bgColor="yellow.100" name={'acumuladoKilos'} value={formData.acumuladoKilos} onChange={handleChange}/>
+                                    <FormLabel className="fw-bold text-center">Acum/Kilos</FormLabel>
+                                    <Input className={'text-center'} type="text" bgColor="yellow.300" name={'acumuladoKilos'} value={formData.acumuladoKilos} onChange={handleChange}/>
                                 </FormControl>
                             </Flex>
                         </Box>
-                        <Box height="auto" bg="white" p="4" boxShadow="xs" rounded="md" flex="0.15">
+                        <Box height="auto" bg="white" p="4" boxShadow="xs" rounded="md" >
                             <Text className="p-3 text-bg-dark text-center">Fator Base</Text>
                             <Flex gap={3} flexWrap="nowrap" p={4}>
-
-
-
                                 <FormControl id="gusaKg" className="form-control">
-                                    <FormLabel className="fw-bold text-center">Densidade Seca</FormLabel>
-                                    <Input className={'text-center'} type="text" bgColor="yellow.100" name={'fatorBaseDensidadeSeca'} value={formData.fatorBaseDensidadeSeca} onChange={handleChange}/>
+                                    <FormLabel className="fw-bold text-center">Dens/Seca</FormLabel>
+                                    <Input className={'text-center'} type="text" bgColor="yellow.300" name={'fatorBaseDensidadeSeca'} value={formData.fatorBaseDensidadeSeca} onChange={handleChange}/>
                                 </FormControl>
                             </Flex>
                         </Box>
                     </Stack>
-
-
-
                 </SimpleGrid>
+
+
                 <SimpleGrid>
                     <Flex className={'mt-3'} justifyContent={'flex-end'}>
                         <Button colorScheme="whatsapp" size="lg" type="submit">
